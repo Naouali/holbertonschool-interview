03f26298e0f30b0b2d5b76fbeed73d7618e5bdd2
Naoualli <elnaoualinebil@gmail.com>
2021-01-18 17:25:22 +0000

    palindrome

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * is_palindrome - fucntion to check linked list is palindrome
+ * @head: head of the linked list
+ * Return: 1 if palindrome 0 if not
+ */
+
+int is_palindrome(listint_t **head)
+{
+    int len = 0;
+    listint_t *h = malloc(sizeof(listint_t));
+    h = *head;
+    int arr1[len/2];
+    while (h != NULL)
+    {
+        h = h->next;
+        len++;
+    }
+    h = *head;
+    int i;
+    for (i = 0; i < len/2; i++)
+    {
+        arr1[i] = h->n;
+        h = h->next;
+    }
+    int j;
+    int pal = 0;
+    for (j = len/2 - 1; j >= 0; j--)
+    {
+        if (h->n == arr1[j])
+        {
+            pal++;
+        }
+        h = h->next;
+    }
+    if (pal == len / 2)
+    {
+        return (1);
+    }
+    return 0;
+}
