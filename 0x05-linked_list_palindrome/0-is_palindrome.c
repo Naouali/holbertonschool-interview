5797b91d8c636b697c24c1172d49a2ae103cb3b7
Naouali <elnaoualinebil@gmail.com>
2021-01-24 14:18:43 +0100

    push

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -12,21 +12,20 @@ int is_palindrome(listint_t **head)
     int len = 0;
     listint_t *h = malloc(sizeof(listint_t));
     h = *head;
-    int arr1[len/2];
+    int *arr1 = malloc(sizeof(int) * len/2);
+    int i, j, pal;
     while (h != NULL)
     {
         h = h->next;
         len++;
     }
     h = *head;
-    int i;
+
     for (i = 0; i < len/2; i++)
     {
         arr1[i] = h->n;
         h = h->next;
     }
-    int j;
-    int pal = 0;
     for (j = len/2 - 1; j >= 0; j--)
     {
         if (h->n == arr1[j])
