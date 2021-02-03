d0adb6ba97f5febb65c72dcaa4f3384c6701f875
Naouali <elnaoualinebil@gmail.com>
2021-02-03 14:12:39 +0100

    scanner

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+stats code
+"""
+
+
+import sys
+
+size = 0
+i = 0
+status = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+          "404": 0, "504": 0, "500": 0}
+for line in sys.stdin:
+    i += 1
+    size += int(line.split()[-1])
+    stat = line.split()[-2]
+    for k in status:
+        if stat == k:
+            status[k] += 1
+
+    if i == 9:
+        print("File size: {}".format(size))
+        for k in status:
+            print("{} : {}".format(k, status[k]))
+    if i == 10:
+        i = 0
+        size = 0
+        for k in status:
+            status[k] = 0
