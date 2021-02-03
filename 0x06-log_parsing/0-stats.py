785957f02607396583ebd69ba3099402b9aa494d
Naouali <elnaoualinebil@gmail.com>
2021-02-03 15:36:51 +0100

    10

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -11,12 +11,14 @@ i = 0
 status = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
           "404": 0, "504": 0, "500": 0}
 for line in sys.stdin:
-    i += 1
+    if line == "":
+        pass
     try:
         size += int(line.split()[-1])
         stat = line.split()[-2]
     except:
         pass
+    i += 1
     for k in status:
         if stat == k:
             status[k] += 1
