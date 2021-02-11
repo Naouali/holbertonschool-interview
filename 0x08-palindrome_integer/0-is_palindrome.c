791ac5f40649ac2e75a3a56486a75497d0ba1b1b
Naouali <elnaoualinebil@gmail.com>
2021-02-11 17:27:34 +0100

    without betty

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -4,62 +4,41 @@
 
 int is_palindrome(unsigned long n)
 {
-    int j = 0;
-    int i = 0;
-    unsigned long rem;
+    unsigned long i, j = 0;
+    unsigned long rem = 0;
     unsigned long rev = 0;
     unsigned long hold;
-    unsigned long hold_right;
+    unsigned long hold2;
     hold = n;
-    hold_right = n;
+    hold2 = n;
     while (hold)
     {
         hold /= 10;
         j++;
     }
-    if (j % 2 == 0)
+    if (j == 1)
     {
+        return (1);
+    }
 
+    i = 0;
+    while (n > 10 && i < j / 2)
+    {
+        rem = n % 10;
+        rev = rev * 10 + rem;
+        n /= 10;
+        i++;
+        hold2 /= 10;
+        if ((i == (j / 2) - 1 && j % 2 != 0) || (j == 3))
+        {
+            hold2 /= 10;
+        }
+    }
+    if (hold2 == rev)
+    {
+        return (1);
+    }
     
-    while (n > 0 && i < j / 2)
-    {
-        rem = n % 10;
-        rev = rev * 10 + rem;
-        n /= 10;
-        i++;
-    }
-    i = 0;
-    while (hold_right && i < j / 2)
-    {
-        i++;
-        hold_right /= 10;
-
-    }
-    if (hold_right == rev)
-        return 1;
-    }
-    else
-    {
-    while (n > 0 && i < j / 2)
-    {
-        rem = n % 10;
-        rev = rev * 10 + rem;
-        n /= 10;
-        i++;
-    }
-    i = 0;
-    printf("len = %d\n", j / 2);
-    while (hold_right && i < j / 2 + 1)
-    {
-        i++;
-        hold_right /= 10;
-
-    }
-    if (hold_right == rev)
-        return 1;
-    }
-
     return 0;
-
 }
-
+        
