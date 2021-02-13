467e246e5b115af839cd6d6e6ae2e573553a59c0
Naouali <elnaoualinebil@gmail.com>
2021-02-13 14:01:49 +0100

    min ops

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Minimim operations
+"""
+
+
+
+def minOperations(n):
+    """
+    minimum ops
+    """
+    count = 0
+    inp = "H"
+    if n < 2 or type(n) is not int:
+        return 0
+    while len(inp) != n:
+        if n % len(inp) == 0:
+            prev = inp
+            inp = inp * 2
+            count += 2
+        else:
+            inp = inp + prev
+            count += 1
+    return count
