0f0632423f1d75daff8ba14609b7209a14401671
Naouali <elnaoualinebil@gmail.com>
2021-02-15 14:40:10 +0100

    check

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Utf 8 validation
+"""
+import sys
+
+def validUTF8(data):
+    """
+    validate utf8 data
+    """
+    Binaries = []
+    for item in data:
+        Binaries.append(bin(item)[2:])
+    for item in Binaries:
+        if len(item) > 7:
+            return False
+        if 192 <= int(item, 2) <= 193:
+            return False
+        if 245 <= int(item, 2) <= 255:
+            return False
+    return True
+
