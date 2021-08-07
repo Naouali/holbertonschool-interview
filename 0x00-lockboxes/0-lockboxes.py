1776a5b843837c047631390345c4fd4addfbd2fb
Naouali <elnaoualinebil@gmail.com>
2021-08-07 21:29:49 +0200

    Inhancement

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,27 +3,50 @@
 lockboxes
 """
 
+def indexmatch(aList):
+    """
+    if the element is equal to it's index, return True, else return false
+    """
+    for i in aList:
+        for j in i:
+            if j == aList.index(i):
+                return True
+    return False
+
+
+
 
 def canUnlockAll(boxes):
     """
-    function to unlock boxes
+    Method to unlock check if boxes are unlocked or locked
+    return True if they can be unlocked, otherwise return false
     """
-    locked = [1] + [0] * (len(boxes) - 1)
-    if len(boxes) == 1:
-        return True
-    if len(boxes) == 0:
-        return False
-    if type(boxes) != list:
-        return False
-    for box in boxes:
-        for item in box:
-            if type(item) is int:
-                if item < len(boxes) and boxes[item]:
-                    locked[item] = 1
+    reference = [0] * (len(boxes))
+    
+    for boxe in boxes:
+        for keys in boxe:
+            if keys >= len(boxes):
+                continue
+            if keys == boxes.index(boxe):
+                continue
             else:
-                return False
-    while locked[-1] == 0:
-        locked.pop()
-    if 0 in locked:
+                reference[keys] += 1
+    
+    if 0 in reference[1:]:
         return False
     return True
+    
+
+
+
+
+
+
+
+
+
+
+
+
+   
+  
