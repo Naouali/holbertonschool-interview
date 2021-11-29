e886bbd3b6f9dd13e90802e82a027be6fdc74b7f
Naouali <elnaoualinebil@gmail.com>
2021-11-29 19:48:09 +0100

    wildstart

---
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/*
+* wildcmp - function to comparea two string, wildstart can hold place
+* for any letter
+* @s1: first string
+* @s2: secon string
+* @Return: 1 if match else 0
+*/
+
+int wildcmp(char *s1, char *s2) {
+
+    
+
+}
---
+++ 0x1E-wild_cmp/holberton.h
@@ -0,0 +1,6 @@
+#IFNDEF header
+#DEFINE header
+
+int wildcmp(char *s1, char *s2);
+
+#ENDIF
